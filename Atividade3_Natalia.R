f24068e5a7568a9a64276972661f7ad81aa037ef
NATALIA SALES MESQUITA <43176292+nataliasm23@users.noreply.github.com>
2019-05-01 22:33:52 -0300

    Add files via upload

---
+++ Atividade3_Natalia.R
@@ -0,0 +1,29 @@
+##############################################################
+# Natália Mesquita
+# Intro a Ciencias de Dados
+#############################################################
+#Exercício 6: As pacientes diagnosticadas com câncer de mama precocemente têm
+#80% de probabilidade de serem completamente curadas. Para um grupo de 12
+#pacientes nessas condições, calcule a probabilidade:
+#  a) Oito ficarem completamente curadas.
+dbinom(x = 8, size = 12, prob = 0.8)
+
+#  b) Serem curadas de 7 a 9 pacientes.
+dbinom( 7, 12, 0.8 ) +
+dbinom( 8, 12, 0.8 ) + 
+dbinom( 9, 12, 0.8 )
+
+#  c) No mínimo 10 completamente curadas.
+pbinom(q = 9, size = 12, prob = 0.8, lower.tail = FALSE)
+
+#  d) O número esperado de curados e seu desvio-padrão
+E<- 12*0.8
+E
+
+SD <-sqrt (E*0.2)
+SD
+
+#7)Em momentos de pico, a chegada de aviões a um aeroporto se dá segundo o modelo
+#Poisson com taxa de 1 por minuto.
+#Determine a probabilidade de 3 chegadas em um minuto qualquer do horário de pico
+ppois(q = 3, lambda = 1)
